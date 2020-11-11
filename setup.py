538feb6fb44abdd5ac1b2870b4d7cde039cf4f05
x041 <x041@users.noreply.github.com>
2020-11-11 13:08:51 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='dynamodb-prettyparser',
-    version='1.0.2',
+    version='1.1.0',
     description='Parses dynamodb responses into a list dictionaries with attribute names as keys',
     author='Mathew Moon',
     author_email='mmoon@quinovas.com',
-    url='https://github.com/QuiNovas/dynamodb-prettyparser',
+    url='https://github.com/x041/dynamodb-prettyparser',
     license='Apache 2.0',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     long_description_content_type='text/x-rst',
